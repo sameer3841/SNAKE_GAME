40277ddb2cff325c811ae2cd40918daca873831a
sameer3841 <81244405+sameer3841@users.noreply.github.com>
2022-05-26 11:12:13 -0400

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -7,7 +7,7 @@ import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
 
-    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_WIDTH = 900;
     static final int SCREEN_HEIGHT = 600;
     static final int UNIT_SIZE = 25;
     static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
